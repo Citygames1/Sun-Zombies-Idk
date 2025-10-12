c424c41f43758d008852ce134647bbcba542d140
Citysuperstar29 <citysuperstar29@gmail.com>
2025-10-12 13:34:48 +0100

    Made it so that points increase in a coroutine

--- Assets/Scripts/Player/Game Scripts/PointSystem.cs
+++ Assets/Scripts/Player/Game Scripts/PointSystem.cs
@@ -8,16 +8,37 @@ public class PointSystem : MonoBehaviour
     public int startingPoints;
     public float totalPoints;
 
+    private float endPointsToGain;
+    private float currentPointAdditionQueue;
+
+    //text increase coroutine
+    public float numIncreaseSpeed = 0.001f;
+    private Coroutine scoreIncreaseCoroutine;
+    [HideInInspector] public bool canContinueIncreasing;
+
     private void Start()
     {
         difficultyManager = GameObject.FindGameObjectWithTag("DifficultyManager").GetComponent<DifficultyManager>();
         totalPoints = startingPoints;
+        endPointsToGain = 0;
     }
     public void GivePoints(int pointsToGain)
     {
         //multiplies the points gained by the difficulty multiplier and gives a rounded value so you dont get a float number
-        float endPointsToGain = Mathf.Round(pointsToGain * difficultyManager.pointsMultiplier);
+        endPointsToGain = Mathf.Round(pointsToGain * difficultyManager.pointsMultiplier);
+        currentPointAdditionQueue += endPointsToGain;
 
-        totalPoints = totalPoints + endPointsToGain;
+        //starts the score increase effect
+        scoreIncreaseCoroutine = StartCoroutine(ScoreIncreaseEffect());
+    }
+
+    public IEnumerator ScoreIncreaseEffect()
+    {
+        while(currentPointAdditionQueue > 0)
+        {
+            totalPoints += 1;
+            currentPointAdditionQueue -= 1;
+            yield return null; //this makes it so that it loops every frame, rather than waiting any seconds
+        }
     }
 }
